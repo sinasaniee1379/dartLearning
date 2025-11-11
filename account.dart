43d96f1b8040717ea4d1de1421bc9d946a9fc993
sina saniee <sinasan67@gmail.com>
2025-11-11 12:51:53 +0330

    nullish operator

---
+++ account.dart
@@ -0,0 +1,4 @@
+class Account {
+  int? number = null;
+  String? name = null;
+}
--- main.dart
+++ main.dart
@@ -1,6 +1,15 @@
+import 'account.dart';
 import 'post.dart';
 
 void main() {
   var newPost = Post();
-  print(newPost.likes);
+  var account = Account();
+  String? newName = null;
+  int len = newName?.length ?? 0;
+  if (newName != null) {
+    int len = newName.length;
+  }
+  // if (account.name != null) {
+  //   int len = account.name!.length;
+  // }
 }
